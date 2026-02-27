c51f2c81e67117a7271d064184cac48b3b98580e
MarcusDavidG <davidmarcus827@gmail.com>
2026-02-27 05:11:42 +0100

    feat(nft-marketplace): add requests query 20901

--- add-1000-queries.sh
+++ add-1000-queries.sh
@@ -3,22 +3,22 @@
 cd /home/marcus/b-stacks
 
 PROJECTS=("staking-pool" "nft-marketplace" "token-vault" "oracle-feed" "dao-voting")
-TYPES=("metrics" "indicators" "measurements" "statistics" "counters" "trackers" "monitors" "sensors" "detectors" "recorders")
+TYPES=("queries" "requests" "responses" "messages" "commands" "instructions" "directives" "orders" "actions" "tasks")
 
 echo "Adding 1000 commits..."
 
 for i in {1..1000}; do
   PROJECT=${PROJECTS[$((i % 5))]}
   TYPE=${TYPES[$((i % 10))]}
-  NUM=$((i + 16900))
+  NUM=$((i + 20900))
   
   mkdir -p $PROJECT/contracts/$TYPE
-  cat > $PROJECT/contracts/$TYPE/metric-$NUM.clar << EOF
-(define-public (metric-$NUM (value uint))
-  (ok (+ value u$NUM)))
+  cat > $PROJECT/contracts/$TYPE/query-$NUM.clar << EOF
+(define-read-only (query-$NUM (id uint))
+  (ok {id: id, result: u$NUM}))
 EOF
   
-  git add . && git commit -m "feat($PROJECT): add $TYPE metric $NUM" --quiet
+  git add . && git commit -m "feat($PROJECT): add $TYPE query $NUM" --quiet
   
   if [ $((i % 100)) -eq 0 ]; then
     echo "Progress: $i/1000 commits"
